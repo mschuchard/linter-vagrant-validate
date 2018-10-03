1baf4216d6ca7d119fbbf30ba91e634251c407d0
Matt Schuchard <grapheneowns@gmail.com>
2018-10-03 19:57:06 -0400

    updating tests/ci for newer vagrant

--- spec/linter-vagrant-validate-spec.js
+++ spec/linter-vagrant-validate-spec.js
@@ -40,7 +40,7 @@ describe('The Vagrant Validate provider for Linter', () => {
           expect(messages[0].severity).toBeDefined();
           expect(messages[0].severity).toEqual('error');
           expect(messages[0].excerpt).toBeDefined();
-          expect(messages[0].excerpt).toEqual("in `block in <top (required)>': undefined local variable or method `cnfig' for main:Object (NameError)");
+          expect(messages[0].excerpt).toEqual("undefined local variable or method `cnfig'");
           expect(messages[0].location.file).toBeDefined();
           expect(messages[0].location.file).toMatch(/.+block_arg\/Vagrantfile$/);
           expect(messages[0].location.position).toBeDefined();
